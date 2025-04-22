318fa673727edfff9c69f7928127359a00d36256
Ali Rizvi <125908944+alrizvi@users.noreply.github.com>
2025-04-22 18:49:49 -0400

    Update producto.py

--- app/schemas/producto.py
+++ app/schemas/producto.py
@@ -6,7 +6,7 @@ class ProductoBase(BaseModel):
     IdCategoria: int = Field(..., alias="IdCategoria")
     IdUnidadMedida: int = Field(..., alias="IdUnidadMedida")
     Nombre: str = Field(..., max_length=100, alias="Nombre")
-    UrlImagen: str = Field(..., alias="UrlImagen")
+    UrlImagen: Optional[str] = Field(None, alias="UrlImagen")
     Descripcion: Optional[str] = Field(None, max_length=300, alias="Descripcion")
 
 
